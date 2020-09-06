66f429fa8004943c09c8d0d2fd06774f1ae039ee
Rajat Dangat <31802904+colton75@users.noreply.github.com>
2020-09-06 20:10:21 +0530

    Add files via upload

--- MMM-Intrusion-Detection.js
+++ MMM-Intrusion-Detection.js
@@ -5,22 +5,6 @@ Module.register("MMM-Intrusion-Detection", {
 
   start: function () {
     Log.log(this.name + " is started!");
-  },
-
-  notificationReceived: function (notification, payload, sender) {
-    if (notification === "ACTIVATE") {
-      if (!this.config.active) {
-        //console.log("Intrusion System Activated............");
-        this.sendSocketNotification("ACTIVATE", "activated");
-        this.config.active = true;
-      }
-    }
-    if (notification === "DEACTIVATE") {
-      if (this.config.active) {
-        //console.log("Intrusion System Dectivated............");
-        this.sendSocketNotification("DEACTIVATE", "deactivated");
-        this.config.active = false;
-      }
-    }
+    this.sendSocketNotification('SET_CONFIG', this.config);
   },
 });
--- node_helper.js
+++ node_helper.js
@@ -1,18 +1,36 @@
 var NodeHelper = require("node_helper");
 const { spawn } = require("child_process");
-
+var config;
 
 module.exports = NodeHelper.create({
-  socketNotificationReceived: function (notification, payload) {
-    if (notification === "ACTIVATE") {
-      console.log(payload);
+
+  start: function() {
+
+    this.expressApp.get('/api/intrusion/activate', (req, res) => {
       const process = spawn("python", ["./modules/MMM-Intrusion-Detection/python/intrusion_det.py"]);
       process.stdout.on("data", (data) => console.log(data.toString()));
       process.stderr.on("data", (data) => console.log("ERROR:",data.toString()));
-      process.on("close", (data) => console.log("Python program ended"));
-    } else if (notification === "DEACTIVATE") {
-      console.log(payload);
+      process.on("close", (data) => {console.log("Python program ended"); config.active=false});
+      config.active = true;
+      res.send("Intrusion system activated");
+    })
+
+    this.expressApp.get('/api/intrusion/deactivate', (req, res) => {
       const process = spawn("pkill", ["-2","-f","python ./modules/MMM-Intrusion-Detection/python/intrusion_det.py"]);
+      //config.active = false;
+      res.send("Intrusion system deactivated");
+    })
+
+    this.expressApp.get('/api/intrusion/isactive', (req, res) => {
+      //console.log("check if active: ",config);
+      res.send({active: config.active});
+    })
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "SET_CONFIG") {
+      //console.log(payload)
+      config = payload;
     }
   },
 });
